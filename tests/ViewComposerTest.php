454afff3a49462ff28ea3d58e913949af93e0923
Vuong Minh <vuongxuongminh@gmail.com>
2019-06-19 14:14:17 +0700

    Added native view composer test

--- tests/ViewComposerTest.php
+++ tests/ViewComposerTest.php
@@ -10,12 +10,21 @@ namespace VXM\MobileFirst\Tests;
 
 class ViewComposerTest extends TestCase
 {
-    public function testAutoSwitchRender()
+    public function testViewAutoSwitchRender()
     {
         $this->app['request']->server->add([
             'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
         ]);
         $result = (string) $this->app['view']->make('test');
+        $this->assertContains('mobile hello world', $result);
+    }
+
+    public function testNativeAutoSwitchRender()
+    {
+        $this->app['request']->server->add([
+            'HTTP_USER_AGENT' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148',
+        ]);
+        $result = (string) $this->app['view']->file(__DIR__.'/resources/views/test.blade.php');
         $this->assertContains('mobile hello world', $result);
     }
 
